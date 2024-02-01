f481906871a63b5ef294ab8fbd929ac62a8b05b8
Nikita Borodins <borodinsnikita@gmail.com>
2024-02-01 11:38:37 +0000

    Add project files.

---
+++ AsianRestaurantProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AsianRestaurantProject.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
